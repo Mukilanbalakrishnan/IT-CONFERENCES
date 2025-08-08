43f2df98a41105a74fe8da82427cbb1618fb3327
hariharan152578 <hariharan152578@gmail.com>
2025-08-08 22:06:04 +0530

    uploade folder stucture

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+
+const AppRoutes = () => {
+  return (
+       <BrowserRouter>
+       <Routes>
+        <Route index to='/' element={<Home/>}/>
+       </Routes>
+       </BrowserRouter>
+  )
+}
+
+export default AppRoutes
