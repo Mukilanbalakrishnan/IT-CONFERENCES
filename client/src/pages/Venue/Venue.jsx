7568c0a991ea742489f58d37516a8c2fb22fe481
Mukilanbalakrishnan <154340556+Mukilanbalakrishnan@users.noreply.github.com>
2025-09-30 10:07:01 +0530

    token-fix

--- client/src/pages/Venue/Venue.jsx
+++ client/src/pages/Venue/Venue.jsx
@@ -7,21 +7,21 @@ const eventData = [
         id: 'evt1',
         type: 'event',
         name: 'Inauguration Ceremony',
-        venue: 'KSR Auditorium',
+        venue: 'Platinum Hall',
         time: '09:30 AM',
-        description: 'The official opening of the Global Summit 2025, featuring a welcome address by the Chairman.',
+        description: 'The official opening of the Joint-Conference, featuring a welcome address by the Chairman.',
         image: 'https://placehold.co/600x400/0D47A1/FFFFFF?text=KSR+Auditorium',
-        map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3911.644171271156!2d77.8489028152769!3d11.39958939192667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba964036a282d51%3A0x32ed249929221163!2sK.S.Rangasamy+College+of+Technology!5e0!3m2!1sen!2sin!4v1517838243628" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+        map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d977.9196700916191!2d77.83117572843234!3d11.358163814138777!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba964016fddc3ad%3A0x672cf11184055521!2sK%20S%20R%20Library%20Block!5e0!3m2!1sen!2sin!4v1759117821591!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
     },
     {
         id: 'evt2',
         type: 'event',
-        name: 'Keynote by Dr. Maya Singh',
-        venue: 'Main Conference Hall',
+        name: 'Keynote by Dr. Malathy Batumalay',
+        venue: 'Platinum Hall',
         time: '11:00 AM',
-        description: 'A thought-provoking keynote address by renowned AI researcher Dr. Maya Singh.',
+        description: 'A thought-provoking keynote address by  Dr. Malathy Batumalay.',
         image: 'https://placehold.co/600x400/F57C00/FFFFFF?text=Conference+Hall',
-        map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3911.644171271156!2d77.8489028152769!3d11.39958939192667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba964036a282d51%3A0x32ed249929221163!2sK.S.Rangasamy+College+of+Technology!5e0!3m2!1sen!2sin!4v1517838243628" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+        map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d977.9196700916191!2d77.83117572843234!3d11.358163814138777!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba964016fddc3ad%3A0x672cf11184055521!2sK%20S%20R%20Library%20Block!5e0!3m2!1sen!2sin!4v1759117821591!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
     },
 ];
 
@@ -29,24 +29,24 @@ const hostelData = [
     {
         id: 'hst1',
         type: 'hostel',
-        name: "Cauvery Men's Hostel",
+        name: "Marina Hostel(Men)",
         description: "A well-furnished hostel for men with modern amenities, including Wi-Fi and 24/7 security.",
         breakfast: '7:30 - 8:30 AM',
         lunch: '12:30 - 1:30 PM',
         dinner: '7:30 - 8:30 PM',
         image: 'https://placehold.co/600x400/1976D2/FFFFFF?text=Men%27s+Hostel',
-        map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3911.644171271156!2d77.8489028152769!3d11.39958939192667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba964036a282d51%3A0x32ed249929221163!2sK.S.Rangasamy+College+of+Technology!5e0!3m2!1sen!2sin!4v1517838243628" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+        map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1331.1081425384627!2d77.82446350859206!3d11.35574923241508!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba9640355489f55%3A0x19eac819d527120a!2sKSR%20Institute%20Rd%2C%20Tamil%20Nadu%20637215!5e0!3m2!1sen!2sin!4v1759118647132!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
     },
     {
         id: 'hst2',
         type: 'hostel',
-        name: "Bhavani Women's Hostel",
+        name: "Tiruchy Hostel(Women)",
         description: "A secure and comfortable hostel for women, offering a pleasant living environment.",
         breakfast: '7:30 - 8:30 AM',
         lunch: '12:30 - 1:30 PM',
         dinner: '7:30 - 8:30 PM',
         image: 'https://placehold.co/600x400/D32F2F/FFFFFF?text=Women%27s+Hostel',
-        map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3911.644171271156!2d77.8489028152769!3d11.39958939192667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba964036a282d51%3A0x32ed249929221163!2sK.S.Rangasamy+College+of+Technology!5e0!3m2!1sen!2sin!4v1517838243628" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+        map: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1861.1779644246215!2d77.82889324580088!3d11.359029675596998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba964022b418327%3A0x5bc1d1b180fa4c8e!2s9R5H%2BWV9%20KSR%20New%20ladies%20hostel%202%2C%20Tamil%20Nadu%20637215!5e0!3m2!1sen!2sin!4v1759118745710!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
     },
 ];
 
@@ -207,4 +207,3 @@ const Venue = () => {
 };
 
 export default Venue;
-
